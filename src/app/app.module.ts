7c24178c0b4dc855b1eb3c73b92e08b6ef3be41d
Dominic Watson <dominic.watson@xcaliber.com>
2017-10-12 09:44:45 +0200

    Update packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent, AppService } from 'angular-cli-red';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([]),
+    AppRoutingModule,
   ],
   providers: [
     AppService,

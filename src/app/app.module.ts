8a35fe1dc5bcfdc1508336fd137f645b6856aed7
Joao Dias <joao.dias@cherrygroup.com>
2017-11-16 15:57:03 +0100

    add a blue button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent, AppService } from 'angular-cli-red';
 import { HomeComponent } from './home/home.component';
+import { BlueButtonComponent } from './blue-button/blue-button';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
+    BlueButtonComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/blue-button/blue-button.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'blue-button',
+  template: '<button>Blue</button>',
+})
+export class BlueButtonComponent {
+}
